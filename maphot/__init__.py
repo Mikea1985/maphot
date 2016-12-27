94b31d6499a541ed1cd3da74d2979b928b390c23
Mike Alexandersen (on Vancouver) <mikea@asiaa.sinica.edu.tw>
2016-12-27 15:38:02 +0800

    Slowly turning into a package

---
+++ maphot/__init__.py
@@ -0,0 +1,7 @@
+"""
+maphot is meant for measuring photometry of a moving object
+accross multiple images of the same field.
+"""
+__version__ = '0.1.1'
+__author__ = 'Mike Alexandersen (github: mikea1985)'
+__all__ = ['maphot', 'photcor', 'best']
